f30e254c73ec6171d1ce87db6197e56b0240be46
Marc <defjam9007@gmail.com>
2020-09-09 14:40:05 +0200

    Add files and description

---
+++ MMM-VrrApp.js
@@ -0,0 +1,80 @@
+/* global Module */
+
+/* Magic Mirror
+ * Module: MMM-VrrApp
+ *
+ * By Marc Helpenstein <helpi9007@gmail.com>
+ * MIT Licensed.
+ */
+
+Module.register("MMM-VrrApp", {
+    defaults: {
+        updateInterval: 60000, // 1 minute
+        retryDelay: 30000, // 30 seconds
+        station: "Düsseldorf Hbf",
+        platform: '',
+        via: '',
+        height:"600px",
+		width:"400px",
+    },
+
+    requiresVersion: "2.1.0",
+    
+    /**
+     * @description Helper function to generate API url
+     * 
+     * @returns {String} url
+     */
+    gennerateUrl: function() {
+        return "https://dbf.finalrewind.org/"+ this.config.station + "?platforms=" + this.config.platform + "&via=" + this.config.via +"&hide_opts=1";
+    },
+
+    /**
+     * @description Calls updateIterval
+     */
+    start: function () {
+        //Flag for check if module is loaded
+        this.loaded = false;
+        // Schedule update timer.
+        setInterval(function () {
+            this.updateDom();
+        }, this.config.updateInterval);
+    },
+
+    /**
+     * @description Schedule next update.
+     * @param {int} delay - Milliseconds before next update.
+     */
+    scheduleUpdate: function (delay) {
+        let nextLoad = this.config.updateInterval;
+        if (typeof delay !== "undefined" && delay >= 0) {
+            nextLoad = delay;
+        }
+        setTimeout(function () {
+            this.updateApp();
+        }, nextLoad);
+    },
+
+    /**
+     * @description Update App
+     */
+    updateApp: function() {
+		this.src = this.vrrAppUrl();
+		this.updateDom(this.config.animationSpeed);
+		this.scheduleUpdate(this.config.refreshInterval);
+    },
+
+    /**
+     * @description Create App Frame
+     * 
+     * @returns {HTMLIframeElement}
+     */
+    getDom: function () {
+        var iframe = document.createElement("IFRAME");
+        iframe.style = "border:0";
+		iframe.width = this.config.width;
+		iframe.height = this.config.height;
+        iframe.src =  this.gennerateUrl();
+		return iframe;
+    },
+});
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # MMM-VrrApp
+This is a module for the [MagicMirror²](https://github.com/MichMich/MagicMirror/).
+
+Displays the next departure times of Trains, subway and Buses from any city and station in the german federal state North Rhine-Westphalia (VRR).
+
+![App wide](vrrApp_wide.png) &nbsp;&nbsp; ![App high](vrrApp_high.png)
+<br/> <br/> 
+
+![App detail](vrrApp_detail.png)
+
+## Installing the module
+Clone this repository in your `~/MagicMirror/modules/`:
+
+`git clone https://github.com/Defjam121/MMM-VrrApp`
+
+
+To use this module, add the following configuration block to the modules array in the `config/config.js` file:
+```js
+var config = {
+    modules: [
+        {
+            module: 'MMM-VrrApp',
+            position: 'top_right',
+            config: {
+                station: "Düsseldorf Hbf",
+                height:"600px",
+		        width:"400px",
+            }
+        }
+    ]
+}
+```
+
+## Configuration options
+
+| **Option** | **Default** | **Description** |
+| --- | --- | --- |
+| `station` | `"Düsseldorf Hbf"` | *Required* <br/>German Station Name
+| `updateInterval` |  `60000` |*Optional* <br/>How often should the data be fetched. 
+| `platform` | `''` | *Optional* <br/> Only show platform. Supports multiple strings, separated by comma (",")
+| `via` | `false` | *Optional* <br/> Only show routs via. Supports multiple strings, separated by comma (",")
+| `height` | `600px` | *Optional* <br/> The height of the App
+| `width` | `400px` | *Optional* <br/> The width of the App
